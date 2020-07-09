9318e62e7dbf9c98954b9df9cc6785f513e47413
Mihail Karlovski <mihailkarlovski@gmail.com>
2020-07-09 17:37:48 +0200

    Update Startup.cs

--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -60,7 +60,7 @@ namespace BookStore
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.UseCors();
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
            
             app.UseMvc();
