81bc104676239e40ef510af3d7d98285c63a5451
Mihail Karlovski <mihailkarlovski@gmail.com>
2020-07-20 21:10:38 +0200

    Create Application.cs

---
+++ BookStore/Data/Application.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Data
+{
+    public class Application
+    {
+        public int Id { get; set; }
+        public string ApiKey { get; set; }
+        public string Name { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
